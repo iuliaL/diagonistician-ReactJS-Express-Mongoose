157ae60e450d40dee4841543642cf8f77b954669
iulia <iuliamarialungu@yahoo.com>
2019-08-24 16:03:38 +0200

    Transformed QuestionList into a fn

--- client/src/js/components/QuestionList.js
+++ client/src/js/components/QuestionList.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 // router
 import { Route, Redirect } from 'react-router-dom'
@@ -28,60 +27,59 @@ const PrivateAddRoute = ({ onAdd, loggedIn, ...rest }) => {
 	)
 };
 
-class QuestionsList extends Component {
-	static propTypes = {
-		questions: PropTypes.array.isRequired,
-	};
-	render() {
-		const { actions, questions, loading, successMessage, errorMessage } = this.props;
-		const { addQuestion } = actions;
-		const singleQuestion = q =>
-			<div key={q._id}>
-				<LinkWrap to={`/question/${q._id}`}>
-					<Question id={q._id}
-						text={q.text}
-						owner={q.owner.username}
-						createdAt={q.createdAt}
-						updatedAt={q.updatedAt}
-					/>
-				</LinkWrap>
-			</div>;
-
-		const questionList = questions.map(singleQuestion);
-
-		return (
-			<div className="grid-100">
-				<h1 className="name align-center">Diagnostician</h1>
-
-				<Success msg={successMessage} />
-				<Error msg={errorMessage} />
-
-				<Route exact path='/list' render={({ match }) =>
-					<LinkWrap exact to={match.url + "/add"}>
-						<button className="button-primary ask-question-button question-form">Ask a question</button>
-					</LinkWrap>
-				} />
-
-				{/*logged in conditional here*/}
-				<PrivateAddRoute
-					path={this.props.match.url + "/add"}
-					loggedIn={this.props.loggedIn}
-					onAdd={addQuestion}
+function QuestionsList({ actions, questions, loggedIn, loading, successMessage, errorMessage, match }) {
+	const singleQuestion = q =>
+		<div key={q._id}>
+			<LinkWrap to={`/question/${q._id}`}>
+				<Question id={q._id}
+					text={q.text}
+					owner={q.owner.username}
+					createdAt={q.createdAt}
+					updatedAt={q.updatedAt}
 				/>
+			</LinkWrap>
+		</div>;
 
-				{loading ?
-					<Loader text="Loading Cases" /> :
-					(<div>
+	const questionList = questions.map(singleQuestion);
+
+	return (
+		<div className="grid-100">
+			<h1 className="name align-center">Diagnostician</h1>
+
+			<Success msg={successMessage} />
+			<Error msg={errorMessage} />
+
+			<Route exact path='/list' render={({ match }) =>
+				<LinkWrap exact to={match.url + "/add"}>
+					<button className="button-primary ask-question-button question-form">Ask a question</button>
+				</LinkWrap>
+			} />
+
+			{/*logged in conditional here*/}
+			<PrivateAddRoute
+				path={match.url + "/add"}
+				loggedIn={loggedIn}
+				onAdd={actions.addQuestion}
+			/>
+
+			{loading ?
+				<Loader text="Loading Cases..." /> :
+				(
+					<div>
 						<h2>Questions</h2>
 						<hr />
 						<div className="questions">
-							{questionList}
+							{
+								questionList.length > 0
+								? questionList
+								: <p>Nothing was asked yet.</p>
+							}
 						</div>
 					</div>)
-				}
-			</div>
-		)
-	}
+			}
+		</div>
+	)
+
 }
 
 function mapStateToProps({ questions, auth, messages }) {
